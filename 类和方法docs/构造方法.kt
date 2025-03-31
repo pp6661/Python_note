c6199f0f07e024097e66c1d89085c518b5ad8bf1
li lu <3066744884@qq.com>
2025-03-31 19:49:37 +0800

    Create 构造方法.kt

---
+++ 类和方法docs/构造方法.kt
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self,name,age,tel):
+        self.name = name
+        self.age = age
+        self.tel= tel
+
+stu=Student("zhou",31,"18500006666")
+print(stu.name)
+print(stu.age)
+print(stu.tel)
